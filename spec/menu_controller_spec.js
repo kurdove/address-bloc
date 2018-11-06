a9ec973dea2ee901e9ea3e6880918e839ce6dbfc
Evgheni Curdov <kurdov.e@gmail.com>
2018-11-05 20:05:28 -0800

    assignment-02 complete

--- spec/menu_controller_spec.js
+++ spec/menu_controller_spec.js
@@ -16,4 +16,10 @@ describe("MenuController", () => {
           expect(this.menu.getContactCount()).toBe(1)
       });
     });
+
+    describe('#remindMe', ()=>{
+        it('should return a string', ()=>{
+            expect(this.menu.remindMe()).toBe('Learning is a life-long pursuit');
+        });
+    });
   });
