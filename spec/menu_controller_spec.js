efb847970867e09dc2861df9bda053575182a018
Evgheni Curdov <kurdov.e@gmail.com>
2018-11-07 14:34:39 -0800

    checkpoint-03 complete

--- spec/menu_controller_spec.js
+++ spec/menu_controller_spec.js
@@ -5,18 +5,7 @@ describe("MenuController", () => {
     beforeEach(()=>{
         this.menu = new MenuController();
     })
-    describe("#getContactCount()", () => {
- 
-      it("should return 0 when no contacts are in the book", () => {
-        expect(this.menu.getContactCount()).toBe(0);
-      });
-
-      it('should return 1 when there is exactly one contact in the book', ()=>{
-        this.menu.book.addContact("Bob", "555-555-5555");
-        expect(this.menu.getContactCount()).toBe(1);
-      });
-    });
-
+    
     describe('#remindMe', ()=>{
         it('should return a string', ()=>{
             expect(this.menu.remindMe()).toBe('Learning is a life-long pursuit');
