53087ec96fca6fa7f73b5eb95d4121597edd9728
Evgheni Curdov <kurdov.e@gmail.com>
2018-11-07 19:11:53 -0800

    assignment-03 complete

--- db/models/contact.js
+++ db/models/contact.js
@@ -2,7 +2,9 @@
 module.exports = (sequelize, DataTypes) => {
   var Contact = sequelize.define('Contact', {
     name: DataTypes.STRING,
-    phone: DataTypes.STRING
+    phone: DataTypes.STRING,
+    email: DataTypes.STRING
+
   }, {});
   Contact.associate = function(models) {
     // associations can be defined here
